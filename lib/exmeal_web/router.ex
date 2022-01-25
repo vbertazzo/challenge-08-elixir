6bb98210a83d9219167c2f145c8b66d351d1579e
vbertazzo <vbertazzo@gmail.com>
2022-01-25 12:10:14 -0300

    Add uuid checker plug

--- lib/exmeal_web/router.ex
+++ lib/exmeal_web/router.ex
@@ -1,14 +1,16 @@
 defmodule ExmealWeb.Router do
   use ExmealWeb, :router
+  alias ExmealWeb.Plugs.UUIDChecker
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+    plug(UUIDChecker)
   end
 
   scope "/api", ExmealWeb do
-    pipe_through :api
+    pipe_through(:api)
 
-    resources "/meals", MealsController, except: [:new, :edit]
+    resources("/meals", MealsController, except: [:new, :edit])
   end
 
   # Enables LiveDashboard only for development
@@ -22,8 +24,8 @@ defmodule ExmealWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: ExmealWeb.Telemetry
+      pipe_through([:fetch_session, :protect_from_forgery])
+      live_dashboard("/dashboard", metrics: ExmealWeb.Telemetry)
     end
   end
 end
